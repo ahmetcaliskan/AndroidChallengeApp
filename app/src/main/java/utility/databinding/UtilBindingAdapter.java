d1c2739bac76ea21b93ec58495e0dbed57ba9677
ahmetcaliskan <Ac899828160455>
2019-09-19 23:21:46 +0300

    BindingAdapter oluşturuldu.

---
+++ app/src/main/java/utility/databinding/UtilBindingAdapter.java
@@ -0,0 +1,45 @@
+package utility.databinding;
+
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class UtilBindingAdapter {
+
+    @BindingAdapter({"setFont"})
+    public static void setFont(View v, String fontFile) {
+        String path = "fonts/" + fontFile;
+        Typeface face = Typeface.createFromAsset(v.getContext().getAssets(), path);
+
+        if (v instanceof TextView)
+            ((TextView) v).setTypeface(face);
+
+        if (v instanceof Button)
+            ((Button) v).setTypeface(face);
+
+        if (v instanceof EditText)
+            ((EditText) v).setTypeface(face);
+
+        if (v instanceof Switch)
+            ((Switch) v).setTypeface(face);
+
+        if (v instanceof CheckBox)
+            ((CheckBox) v).setTypeface(face);
+
+        if (v instanceof TextInputLayout)
+            ((TextInputLayout) v).setTypeface(face);
+
+        if (v instanceof RadioButton)
+            ((RadioButton) v).setTypeface(face);
+    }
+}
+
