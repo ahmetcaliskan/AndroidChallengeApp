d06dbc12d1ea949e821b4fad3c6d70b8935ed684
ahmetcaliskan <Ac899828160455>
2019-09-20 22:44:38 +0300

    Siparislerim ekranı tasarım olustuldu

---
+++ app/src/main/java/com/c/a/androidchallengeapp/ui/fragment/FragmentOrders.java
@@ -0,0 +1,61 @@
+package com.c.a.androidchallengeapp.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.c.a.androidchallengeapp.R;
+import com.c.a.androidchallengeapp.databinding.FragmentOrdersBinding;
+import com.c.a.androidchallengeapp.model.ModelOrders;
+import com.c.a.androidchallengeapp.viewmodel.ViewModelOrders;
+
+import java.util.List;
+
+public class FragmentOrders extends Fragment {
+    private AppCompatActivity activity;
+    private ViewModelOrders viewModel;
+    private Observer<List<ModelOrders>> observer;
+    private FragmentOrdersBinding binding;
+
+    public static FragmentOrders newInstance() {
+        return new FragmentOrders();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_orders, container, false);
+        View view = binding.getRoot();
+
+        if (getActivity() != null) {
+            activity = (AppCompatActivity) getActivity();
+        }
+
+        viewModel = ViewModelProviders.of(this).get(ViewModelOrders.class);
+        startObserve(viewModel.getOrders());
+
+        return view;
+    }
+
+    private void startObserve(MutableLiveData<List<ModelOrders>> mediatorLiveData) {
+        observer = new Observer<List<ModelOrders>>() {
+            @Override
+            public void onChanged(List<ModelOrders> list) {
+                if (list != null) {
+
+                }
+            }
+        };
+        mediatorLiveData.observe(this, observer);
+    }
+}
