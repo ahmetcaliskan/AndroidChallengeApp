c689669de5a83a5569bf3caa9f80958280ae1f54
ahmetcaliskan <Ac899828160455>
2019-09-22 15:01:06 +0300

    Siparişlerim ekranı databindig list kontrolü için observable alan eklendi.

--- app/src/main/java/com/c/a/androidchallengeapp/viewmodel/ViewModelOrders.java
+++ app/src/main/java/com/c/a/androidchallengeapp/viewmodel/ViewModelOrders.java
@@ -3,6 +3,7 @@ package com.c.a.androidchallengeapp.viewmodel;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableField;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
@@ -12,15 +13,17 @@ import com.c.a.androidchallengeapp.repository.RepositoryOrders;
 import java.util.List;
 
 public class ViewModelOrders extends AndroidViewModel {
-    private MutableLiveData<List<ModelOrders>> listOrders;
     private RepositoryOrders repositoryOrders;
+    public ObservableField<List<ModelOrders>> listOrders;
 
     public ViewModelOrders(@NonNull Application application) {
         super(application);
         repositoryOrders = new RepositoryOrders();
+        listOrders = new ObservableField<>();
     }
 
     public MutableLiveData<List<ModelOrders>> getOrders() {
+        listOrders.set(repositoryOrders.getOrders().getValue());
         return repositoryOrders.getOrders();
     }
 }
